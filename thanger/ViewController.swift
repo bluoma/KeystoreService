88e48ca90da5faf24c269e9b10fa68e404417217
Bill Luoma <bluoma@gmail.com>
2019-10-28 19:12:23 -0700

    Initial Commit

---
+++ thanger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  thanger
+//
+//  Created by Bill on 10/28/19.
+//  Copyright © 2019 Bill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
